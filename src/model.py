6d9bb150ee965d6e11b9bfa024d06bb3be7fdd4f
Gonul Eda Koker <gonul.koker@sjsu.edu>
2026-04-08 20:04:53 -0700

    Fix model classifier architecture to match trained weights, update gitignore
    Closes #6

--- src/model.py
+++ src/model.py
@@ -10,5 +10,8 @@ def create_efficientnet_b0(num_classes: int = 101, pretrained: bool = False) -> 
     model = models.efficientnet_b0(weights=weights)
 
     in_features = model.classifier[1].in_features
-    model.classifier[1] = nn.Linear(in_features, num_classes)
+    model.classifier = nn.Sequential(
+    nn.Dropout(p=0.3, inplace=True),
+    nn.Linear(in_features, num_classes),
+)
     return model
